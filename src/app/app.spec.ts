ad79067d0eefe1c0eb42e450d1b1b561e97348b9
Lucas Yuzo Kihana <lucas.kihana@gmail.com>
2025-06-16 18:33:43 -0300

    config: add vitest

--- src/app/app.spec.ts
+++ src/app/app.spec.ts
@@ -1,5 +1,6 @@
 import { provideZonelessChangeDetection } from '@angular/core';
 import { TestBed } from '@angular/core/testing';
+import { beforeEach, describe, expect, it } from 'vitest';
 import { App } from './app';
 
 describe('App', () => {
